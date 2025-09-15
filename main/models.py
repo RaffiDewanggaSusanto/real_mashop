bb83c8ed839a0e034f0a5c9cfa7aac8d606025df
RaffiDewanggaSusanto <dewangga2209@gmail.com>
2025-09-16 00:31:28 +0700

    Tugas 3: Penambahan Form dan Data Delivery

--- main/models.py
+++ main/models.py
@@ -3,13 +3,12 @@ from django.db import models
 
 class Product(models.Model):
     CATEGORY_CHOICES = [
-        ('name', 'Name'),
-        ('price', 'Price'),
-        ('description', 'Description'),
-        ('thumbnail', 'Thumbnail'),
-        ('category', 'Category'),
-        ('is_featured', 'Is_featured'),
-        ('rating', 'Rating'),
+        ('hat', 'Hat'),
+        ('jersey', 'Jersey'),
+        ('pant', 'Pant'),
+        ('sock', 'Sock'),
+        ('shoe', 'Shoe'),
+        ('boxer', 'Boxer'),
     ]
     
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
@@ -21,14 +20,5 @@ class Product(models.Model):
     is_featured = models.BooleanField(default = False)
     rating = models.IntegerField()
 
-    
     def __str__(self):
         return self.title
-    
-    @property
-    def is_news_hot(self):
-        return self.news_views > 20
-        
-    def increment_views(self):
-        self.news_views += 1
-        self.save()
