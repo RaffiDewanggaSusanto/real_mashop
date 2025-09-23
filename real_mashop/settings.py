3d4fbf689809481dfcf15d3d27c73571a3474c03
RaffiDewanggaSusanto <dewangga2209@gmail.com>
2025-09-23 22:47:02 +0700

    Memperbaiki CSRF_TRUSTED_ORIGINS

--- real_mashop/settings.py
+++ real_mashop/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "raffi-dewangga-realmashop.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://raffi-dewangga-realmashop.pbp.cs.ui.ac.id/"
+    "https://raffi-dewangga-realmashop.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
